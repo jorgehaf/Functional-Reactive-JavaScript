3e2cdb28272a60b44319f441edce34032942f5c1
jorgehaf <jorgehenriqueaf@gmail.com>
2023-06-08 17:51:28 -0300

    Closure

---
+++ functional/closure.js
@@ -0,0 +1,8 @@
+// Closure é quando uma função "lembra"
+// seu escopo léxico, memso quando a função
+// é executada dora desse escopo léxico
+
+const some = require("./closure_escope");
+
+const x = 20;
+console.log(some());
---
+++ functional/closure_escope.js
@@ -0,0 +1,6 @@
+const x = 10;
+function some() {
+  return x + 5;
+}
+
+module.exports = some;
